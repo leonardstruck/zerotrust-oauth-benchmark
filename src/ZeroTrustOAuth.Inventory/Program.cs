912b4264b3b26946157274fc0bb56f17480db757
Leonard Struck <me@leonardstruck.com>
2025-11-24 10:55:17 +0100

    Integrate shared migration logic into Inventory service

--- src/ZeroTrustOAuth.Inventory/Program.cs
+++ src/ZeroTrustOAuth.Inventory/Program.cs
@@ -2,6 +2,7 @@ using Carter;
 
 using FluentValidation;
 
+using ZeroTrustOAuth.Data.Extensions;
 using ZeroTrustOAuth.Inventory.Data;
 using ZeroTrustOAuth.ServiceDefaults;
 
@@ -10,6 +11,7 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 builder.Services.AddOpenApi();
 builder.AddNpgsqlDbContext<InventoryDbContext>("inventorydb");
+builder.AddMigration<InventoryDbContext>();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 builder.Services.AddCarter();
 
